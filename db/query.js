f68d5ccc87d1ea5b8db75ca22db93313342735a2
jekingxu <39491203+jekingxu@users.noreply.github.com>
2020-03-17 12:52:43 +0800

    修移动端兼容性

--- db/query.js
+++ db/query.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    host:"127.0.0.1",
-    // host:"120.79.139.240",
-    user:"root",
-    password:"",
-    database:"socket_chat",
-    port:3306
+    // host:"127.0.0.1",
+    host: "120.79.139.240",
+    user: "root",
+    password: "root",
+    database: "socket_chat",
+    port: 3306
 })
 
-const query = (sql)=>{
-    return new Promise((resolve,reject)=>{
-        pool.query(sql,(err , data ,fileds)=>{
-            if(err){
-                reject("数据库连接错误:",err);
+const query = (sql) => {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, (err, data, fileds) => {
+            if (err) {
+                reject("数据库连接错误:", err);
             }
-            resolve(data,fileds);
+            resolve(data, fileds);
         })
     })
 }
